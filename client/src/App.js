63c50b05f192d5098d660a33e0d2098b76fd765a
Anna Astle <ast19004@byui.edu>
2022-12-06 17:39:48 -0800

    Github & Linkedin Linke

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,41 @@
 import "./App.css";
 import Header from "./Components/Header";
 
+import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className="App-welcome">
+        <section className="intro">
+          <h1>Anna Astle</h1>
+          <span>WEB DEVELOPER</span>
+        </section>
+        <ul className={"bottomNav"}>
+          <li>
+            <ArrowDownwardIcon className="scrollIcon" />
+          </li>
+          <li className="links">
+            <a
+              href="https://www.linkedin.com/in/annaastle-webdeveloper"
+              rel="noreferrer"
+              target="_blank"
+            >
+              <LinkedInIcon />
+            </a>
+            <a
+              href="https://github.com/ast19004"
+              rel="noreferrer"
+              target="_blank"
+            >
+              <GitHubIcon />
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -2,25 +2,10 @@ import MainNav from "./MainNav";
 
 import styles from "./Header.module.css";
 
-import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
-import GitHubIcon from "@mui/icons-material/GitHub";
-import LinkedInIcon from "@mui/icons-material/LinkedIn";
-
 const Header = () => {
   return (
     <header className={styles.header}>
       <MainNav />
-      <div className={styles.intro}>
-        <h1>Anna Astle</h1>
-        <span>WEB DEVELOPER</span>
-      </div>
-      <div className={styles.bottomNav}>
-        <ArrowDownwardIcon className={styles.scrollIcon} />
-        <div className={styles.links}>
-          <LinkedInIcon />
-          <GitHubIcon />
-        </div>
-      </div>
     </header>
   );
 };
