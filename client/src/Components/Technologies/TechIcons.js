7b2bb9691f9089ee52cc06fb12a0a1dd999684dd
Anna Astle <ast19004@byui.edu>
2022-12-11 14:57:39 -0800

    updated alt for icon images

--- client/src/Components/Technologies/TechIcons.js
+++ client/src/Components/Technologies/TechIcons.js
@@ -5,7 +5,7 @@ export const CSharp = () => {
     <ListImage
       opacity="45%"
       src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/csharp/csharp-original.svg"
-      alt="c sharp icon"
+      alt="C sharp"
     />
   );
 };
@@ -15,7 +15,7 @@ export const CSS = () => {
     <ListImage
       opacity="45%"
       src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg"
-      alt="css icon"
+      alt="Css"
     />
   );
 };
@@ -25,7 +25,7 @@ export const Express = () => {
     <ListImage
       opacity="45%"
       src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg"
-      alt="express icon"
+      alt="Express"
     />
   );
 };
@@ -35,7 +35,7 @@ export const GitHub = () => {
     <ListImage
       opacity="35%"
       src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
-      alt="git icon"
+      alt="Github"
     />
   );
 };
@@ -44,7 +44,7 @@ export const HTML5 = () => {
     <ListImage
       opacity="45%"
       src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg"
-      alt="HTML icon"
+      alt="HTML"
     />
   );
 };
@@ -53,7 +53,7 @@ export const Java = () => {
     <ListImage
       opacity="60%"
       src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-plain.svg"
-      alt="Java Icon"
+      alt="Java"
     />
   );
 };
@@ -61,7 +61,7 @@ export const Javascript = () => {
   return (
     <ListImage
       src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg"
-      alt="Javascript Icon"
+      alt="Javascript"
     />
   );
 };
@@ -70,7 +70,7 @@ export const MongoDb = () => {
     <ListImage
       opacity="60%"
       src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg"
-      alt="MongoDb Icon"
+      alt="MongoDb"
     />
   );
 };
@@ -79,7 +79,7 @@ export const MySQL = () => {
     <ListImage
       opacity="60%"
       src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg"
-      alt="SQL Icon"
+      alt="MySQL"
     />
   );
 };
@@ -88,7 +88,7 @@ export const Nodejs = () => {
   return (
     <ListImage
       src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg"
-      alt="node.js icon"
+      alt="Node.js"
     />
   );
 };
@@ -97,7 +97,7 @@ export const PHP = () => {
     <ListImage
       opacity="60%"
       src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-plain.svg"
-      alt="PHP icon"
+      alt="PHP"
     />
   );
 };
@@ -105,7 +105,7 @@ export const Python = () => {
   return (
     <ListImage
       src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg"
-      alt="Python Icon"
+      alt="Python"
     />
   );
 };
@@ -113,7 +113,7 @@ export const React = () => {
   return (
     <ListImage
       src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
-      alt="React Icon"
+      alt="React"
     />
   );
 };
@@ -122,7 +122,7 @@ export const Sass = () => {
     <ListImage
       opacity="60%"
       src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg"
-      alt="Sass Icon"
+      alt="Sass"
     />
   );
 };
