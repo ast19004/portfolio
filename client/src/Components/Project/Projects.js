c19bc990ece632a88da25454a7b29afd4dcf44b9
Anna Astle <ast19004@byui.edu>
2023-05-04 12:47:02 -0700

    Projects in progress temporarily commented out

--- client/src/Components/Project/Projects.js
+++ client/src/Components/Project/Projects.js
@@ -30,7 +30,7 @@ const Projects = () => {
       <Project
         title="Read Reader"
         description="Created with elementary school children and their guardians in mind, this application provides guardians with the ability to track the children's daily reading. Each child has their own personal account within the guardian's account and is able to 'pay' for rewards based on the amount of time they have read. It is up the main user/ guardian to create possible prizes and to decide upon how much time in reading will be required to redeem each prize."
-        href="https://www.google.com"
+        href="https://read-reader.herokuapp.com/"
         src={readerReaderImg}
         alt="Link to Read Reader Application"
       >
@@ -45,7 +45,7 @@ const Projects = () => {
       <Project
         title="Scootz"
         description="This ficticious Scooter Rental Website is built using the React library and demonstrates some playful use of CSS animations."
-        href="https://www.google.com"
+        href="https://ast19004.github.io/scootz/"
         src={scootzImg}
         alt="Link to Scootz Application"
       >
@@ -53,7 +53,7 @@ const Projects = () => {
         <React />
         <CSS />
       </Project>
-
+      {/* 
       <Project
         title="Brain Storm"
         description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id venenatis a condimentum vitae. Enim sit amet venenatis urna cursus. Etiam erat velit scelerisque in dictum non consectetur a erat. Arcu bibendum at varius vel pharetra vel turpis nunc. Risus sed vulputate odio ut enim. Sed libero enim sed faucibus turpis in. Sollicitudin ac orci phasellus egestas tellus rutrum tellus. Nunc id cursus metus aliquam. Elementum nibh tellus molestie nunc non."
@@ -76,7 +76,7 @@ const Projects = () => {
         <HTML5 />
         <CSharp />
         <CSS />
-      </Project>
+      </Project> */}
     </div>
   );
 };
