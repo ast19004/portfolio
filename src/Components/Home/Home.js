0b56bcf8a4cc9e9129cfdb33313db211aad0f754
Anna Astle <ast19004@byui.edu>
2023-07-13 13:24:00 -0700

    Remove temporaily commented out code

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,3 @@
-import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
-import GitHubIcon from "@mui/icons-material/GitHub";
-import LinkedInIcon from "@mui/icons-material/LinkedIn";
-
-import MainLogo from "../MainLogo";
-
 import About from "../About/About";
 import Technologies from "../Technologies/Technologies";
 import AnnaAstleImg from "../../assets/images/annaastle_halfImg.jpg";
@@ -21,34 +15,6 @@ const Home = () => {
           <About />
           <Technologies />
         </div>
-        {/* <div className={styles["intro"]}>
-        <section>
-          <h1 className="decorative font-large">Anna Astle</h1>
-          <span>WEB DEVELOPER</span>
-        </section>
-        <MainLogo className="App-logo" />
-      </div> */}
-        {/* <ul className={styles["bottomNav"]}>
-        <li onClick={() => (window.location.href = "#projects")}>
-          <ArrowDownwardIcon className={styles["scrollIcon"]} />
-        </li>
-        <li className={styles["links"]}>
-          <a
-            href="https://www.linkedin.com/in/annaastle-webdeveloper"
-            rel="noreferrer"
-            target="_blank"
-          >
-            <LinkedInIcon color="action" />
-          </a>
-          <a
-            href="https://github.com/ast19004"
-            rel="noreferrer"
-            target="_blank"
-          >
-            <GitHubIcon color="action" />
-          </a>
-        </li> */}
-        {/* </ul> */}
       </div>
     </Box>
   );
