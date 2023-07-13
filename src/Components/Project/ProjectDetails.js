85e5159a0dbad52c4c1e3b4587ab1d6cc8adc07b
Anna Astle <ast19004@byui.edu>
2023-07-12 22:08:36 -0700

    Add Expertise Bar in About Me

--- src/Components/Project/ProjectDetails.js
+++ src/Components/Project/ProjectDetails.js
@@ -5,8 +5,14 @@ const ProjectDetails = (props) => {
     <section className={props.className}>
       <h2 className="font-medium">{props.title}</h2>
       <p className="gray">{props.description}</p>
-      <Link href={props.github} target="_blank" color="#777" underline="none">
-        <i>{props.github}</i>
+      <Link
+        href={props.github}
+        target="_blank"
+        color="#777"
+        underline="none"
+        sx={{ padding: "5px" }}
+      >
+        <b>View My Code</b>
       </Link>
       <ProjectTechnologies children={props.technologies} />
     </section>
--- src/Components/Technologies/Technologies.js
+++ src/Components/Technologies/Technologies.js
@@ -38,8 +38,53 @@ const Technologies = (props) => {
         <Python />
       </ul>
 
-      <Box sx={{ position: "relative" }} className={styles.expertise}>
-        <Box></Box>
+      <Box className={styles.expertise}>
+        <Box>
+          <Box
+            sx={{
+              height: "2px",
+              backgroundColor: "#BEBABA",
+              marginLeft: "-3px",
+              width: "182px",
+              "@media(min-width: 860px)": { width: "217px" },
+            }}
+          />
+          <Box
+            component="span"
+            sx={{
+              display: "block",
+              marginLeft: "-3px",
+              marginTop: "-1px",
+              fontSize: "0.6rem",
+              color: "#8b8989",
+              "@media(min-width: 860px)": { fontSize: "0.7rem" },
+            }}
+          >
+            Proficient
+          </Box>
+        </Box>
+        <Box>
+          <Box
+            sx={{
+              height: "2px",
+              backgroundColor: "#BEBABA",
+              width: "98px",
+              "@media(min-width: 860px)": { fontSize: "0.7rem" },
+            }}
+          />
+          <Box
+            component="span"
+            sx={{
+              display: "block",
+              marginTop: "-1px",
+              fontSize: "0.6rem",
+              color: "#8b8989",
+              "@media(min-width: 860px)": { fontSize: "0.7rem" },
+            }}
+          >
+            Experienced
+          </Box>
+        </Box>
       </Box>
     </Box>
   );
