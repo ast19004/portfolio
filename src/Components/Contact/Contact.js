34fe2ece2d3effac1048cf43b3f1889070263e71
Anna Astle <ast19004@byui.edu>
2023-07-14 08:58:55 -0700

    May Contact Info Clickable

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -6,29 +6,49 @@ import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 const Contact = () => {
   return (
-    <ul className={styles.contact}>
-      <li className="decorative font-medium">Anna Astle</li>
-      <li className={styles["links"]}>
-        <a
-          href="https://www.linkedin.com/in/annaastle-webdeveloper"
-          rel="noreferrer"
-          target="_blank"
-        >
-          <LinkedInIcon
-            fontSize="large"
-            sx={{ color: "red", opacity: "0.75" }}
-          />
-        </a>
-        <a href="https://github.com/ast19004" rel="noreferrer" target="_blank">
-          <GitHubIcon fontSize="large" sx={{ color: "red", opacity: ".75" }} />
-        </a>
-      </li>
-      <li>annaastle5@gmail.com</li>
-      <li>(323) 629 6604</li>
-      <li>
-        <MainLogo className={`App-logo ${styles["contact-logo"]}`} />
-      </li>
-    </ul>
+    <footer>
+      <ul className={styles.contact}>
+        <li className="decorative font-medium">Anna Astle</li>
+        <li className={styles["links"]}>
+          <a
+            href="https://www.linkedin.com/in/annaastle-webdeveloper"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <LinkedInIcon
+              fontSize="large"
+              sx={{ color: "red", opacity: "0.75" }}
+            />
+          </a>
+          <a
+            href="https://github.com/ast19004"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <GitHubIcon
+              fontSize="large"
+              sx={{ color: "red", opacity: ".75" }}
+            />
+          </a>
+        </li>
+        <li>
+          <a
+            className={styles["contact-email"]}
+            href="mailto:annaastle5@gmail.com"
+          >
+            annaastle5@gmail.com
+          </a>
+        </li>
+        <li>
+          <a className={styles["contact-phone"]} href="tel:323-629-6604">
+            (323) 629 6604
+          </a>
+        </li>
+        <li>
+          <MainLogo className={`App-logo ${styles["contact-logo"]}`} />
+        </li>
+      </ul>
+    </footer>
   );
 };
 
