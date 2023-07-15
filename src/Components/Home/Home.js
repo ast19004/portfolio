17616a961bac748f97666c7d871b9aba7e70daa8
Anna Astle <ast19004@byui.edu>
2023-07-15 16:11:50 -0700

    Add Scroll Indicator

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,13 +1,31 @@
+import { useState, useEffect } from "react";
+import { useInView } from "react-intersection-observer";
+
 import About from "../About/About";
 import Technologies from "../Technologies/Technologies";
 import AnnaAstleImg from "../../assets/images/annaastle_halfImg.jpg";
 import styles from "./Home.module.css";
 import { Box } from "@mui/material";
+import ScrollDownIcon from "@mui/icons-material/South";
 
 const Home = () => {
+  const [inViewStyle, setInViewStyle] = useState("");
+  const { ref, inView } = useInView({
+    threshold: 0.5,
+  });
+
+  useEffect(() => {
+    if (inView) {
+      setInViewStyle("moveDownFadeOut");
+    }
+    return () => setInViewStyle("");
+  }, [inView]);
+
   return (
     <Box
+      ref={ref}
       sx={{
+        position: "relative",
         display: "grid",
         justifyContent: "center",
         alignItems: "center",
@@ -16,6 +34,13 @@ const Home = () => {
         backgroundColor: "#f1f1f1",
       }}
     >
+      <Box
+        className={inViewStyle}
+        component="span"
+        sx={{ opacity: 0, position: "absolute", top: "85%", left: "1%" }}
+      >
+        <ScrollDownIcon fontSize="large" sx={{ color: "#0c77ff" }} />
+      </Box>
       <div className={styles.homeContainer}>
         <div className={styles.colLeft}>
           <img src={AnnaAstleImg} alt="Web Developer Anna Astle" />
