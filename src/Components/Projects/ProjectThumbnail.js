3a296570eef94b86f3aca7b9dbd95edbfdce56b4
Anna Astle <ast19004@byui.edu>
2023-10-10 12:27:48 -0700

    Improvement to Portfolio link animation

--- src/Components/Projects/ProjectThumbnail.js
+++ src/Components/Projects/ProjectThumbnail.js
@@ -12,7 +12,7 @@ const ProjectThumbnail = (props) => {
 
   useEffect(() => {
     if (inView) {
-      setInViewStyle("fadeInAndOut");
+      setInViewStyle("fadeIn");
     }
     return () => setInViewStyle("");
   }, [inView]);
